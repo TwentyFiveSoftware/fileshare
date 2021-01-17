2b5ac60ef4f2c2e547af6abbc8191b47dbf414e4
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-01-17 11:34:29 +0100

    changed loading animation

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from '../styles/LoadingAnimation.module.scss';
 
-const LoadingAnimation = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.dot} />
-            <div className={styles.dot} style={{ animationDelay: '0.1s' }} />
-            <div className={styles.dot} style={{ animationDelay: '0.2s' }} />
-            <div className={styles.dot} style={{ animationDelay: '0.3s' }} />
-            <div className={styles.dot} style={{ animationDelay: '0.4s' }} />
-        </div>
-    );
-};
+const LoadingAnimation = () => (
+    <div className={styles.container}>
+        {(() => {
+            const dots = [];
+
+            for (let i = 0; i < 360; i += 30)
+                dots.push(
+                    <div className={styles.dot}
+                         style={{transform: `translateX(-50%) rotate(${i}deg)`}}
+                         key={i}
+                    >
+                        <div style={{animationDelay: `${i / 300}s`}}/>
+                    </div>
+                );
+
+            return dots;
+        })()}
+    </div>
+);
 
 export default LoadingAnimation;
